01c68332d88ff73f861d7ef5b7cced12a322a6b2
genejones <generjones@gmail.com>
2013-05-16 16:16:34 -0400

    Changed from 1Hz to 2Hz!

--- .build/uno/src/sketch.cpp
+++ .build/uno/src/sketch.cpp
@@ -19,7 +19,7 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
+  delay(500);              // wait for a second
   digitalWrite(13, LOW);    // set the LED off
   delay(1000);              // wait for a second
 }
