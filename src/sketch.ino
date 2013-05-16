c1e3c3d766357a685102b8b1cd6b88d7af036c4d
genejones <generjones@gmail.com>
2013-05-16 16:29:55 -0400

    more serial prints!

--- src/sketch.ino
+++ src/sketch.ino
@@ -16,6 +16,7 @@ void loop() {
   digitalWrite(13, HIGH);   // set the LED on
   delay(1000);              // wait for a second
   digitalWrite(13, LOW);    // set the LED off
+  Serial.print("Off");
   delay(1000);              // wait for a second
-  Serial.print("Blink!");
+  Serial.print("On");
 }
